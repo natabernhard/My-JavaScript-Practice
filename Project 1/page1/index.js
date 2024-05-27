ba304bfec77b01366b62727dabbaaba5c26c78ee
natabernhard <142544209+natabernhard@users.noreply.github.com>
2024-05-27 13:21:42 -0500

    Update index.js

--- Project 1/page1/index.js
+++ Project 1/page1/index.js
@@ -1,12 +1,21 @@
-let kartina = document.getElementById("picture1");
+let picture = document.getElementById("picture1");
+let passage = document.getElementById("container");
 
-// let newText = document.createElement('p');
-// newText.textContent = 'How many lives do cats have?';
-
-kartina.addEventListener("click", function () {
-  kartina.style.display = "none";
-  kartina.remove();
-
-  let container = document.getElementById("container"); // container.appendChild (newText);
-  container.textContent = "How many lives do cats have?";
+passage.addEventListener("click", function () {
+  picture.style.display = "none";
+  passage.textContent = "Hello";
 });
+
+
+// let kartina = document.getElementById("picture1");
+
+// // let newText = document.createElement('p');
+// // newText.textContent = 'How many lives do cats have?';
+
+// kartina.addEventListener("click", function () {
+//   kartina.style.display = "none";
+//   kartina.remove();
+
+//   let container = document.getElementById("container"); // container.appendChild (newText);
+//   container.textContent = "How many lives do cats have?";
+// });
